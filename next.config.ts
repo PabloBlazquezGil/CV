370d0538a6ec53d4ca4578b9e8d16f1b28d09a90
Pablo Blázquez Gil <pabloblazquezgil@gmail.com>
2026-02-04 08:51:19 +0000

    Quiero que pongas esta imagen como foto mía de perfil: https://thundersh

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'thundershoot.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
